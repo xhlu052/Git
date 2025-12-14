1e1f04e4ff7b4dbe50c8906a47af59c09c2674a7
luka <scarsuper@gmail.com>
2025-12-14 15:56:16 +0100

    code is printing

--- index.php
+++ index.php
@@ -10,5 +10,19 @@
 echo "<p>Hello World</p>"; // 'echo' is used to output text to the browser
 ?>
 
+<?php
+class Car {
+    function Car() {
+        $this->model = "Tesla";
+    }
+}
+
+// create an object
+$Lightning = new Car();
+
+// show object properties
+echo $Lightning->model;
+?>
+
 </body>
 </html>
